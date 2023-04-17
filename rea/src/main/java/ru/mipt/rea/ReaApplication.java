afe1e100dff18a2a5407201e9dcad6b2061b0fa6
Aleksandr Semyachkin <aleks.semyachkin@mail.ru>
2023-04-17 13:25:48 +0300

    Initialized Spring project

---
+++ rea/src/main/java/ru/mipt/rea/ReaApplication.java
@@ -0,0 +1,13 @@
+package ru.mipt.rea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaApplication.class, args);
+    }
+
+}
