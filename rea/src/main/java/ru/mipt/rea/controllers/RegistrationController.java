19014d5c3013d3424cd050f0141c5ceb4381e940
alexansemya <aleks.semyachkin@mail.ru>
2023-05-09 13:36:05 +0300

    Working on RegistrationController

--- rea/src/main/java/ru/mipt/rea/controllers/RegistrationController.java
+++ rea/src/main/java/ru/mipt/rea/controllers/RegistrationController.java
@@ -1,6 +1,12 @@
 package ru.mipt.rea.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,12 +20,18 @@ import ru.mipt.rea.exception.UserAlreadyExistsException;
 import ru.mipt.rea.models.User;
 import ru.mipt.rea.service.UserServiceImpl;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
 
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
 
     @ModelAttribute("user")
     public UserDTO userDTO() {
@@ -34,6 +46,7 @@ public class RegistrationController {
     public String registration(@ModelAttribute("user") UserDTO userDTO,
                                RedirectAttributes redirectAttributes,
                                BindingResult bindingResult,
+                               HttpServletRequest request,
                                Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("errors", bindingResult.getAllErrors());
@@ -42,13 +55,20 @@ public class RegistrationController {
 
         try {
             User user = userService.register(userDTO);
-            redirectAttributes.addAttribute("id", user.getId());
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+            request.getSession();
+            authenticationToken.setDetails(new WebAuthenticationDetails(request));
+            Authentication authentication = authenticationProvider.authenticate(authenticationToken);
+            redirectAttributes.addAttribute("id", userDTO.getId());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             redirectAttributes.addFlashAttribute("message", "Registration successful");
-            return "redirect:/home/student/{id}";
+            return "redirect:/home/student/";
 
         } catch (UserAlreadyExistsException exception) {
             bindingResult.rejectValue("username", "error.user", "User with this username already exists");
             return "registration";
         }
     }
+
 }
