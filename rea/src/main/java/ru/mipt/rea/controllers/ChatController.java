d389991ad2b9eb03f874ff0382c0befd205f5f7d
fpmiegorzaytsev <zaitsev.egor@phystech.edu>
2023-05-10 17:04:16 +0300

    working on chats

---
+++ rea/src/main/java/ru/mipt/rea/controllers/ChatController.java
@@ -0,0 +1,20 @@
+package ru.mipt.rea.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    private ChatMessageService chatMessageService;
+
+    @Autowired
+    private ChatRoomService chatRoomService;
+
+}
